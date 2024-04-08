7ac0a76bfb619e09c15f2d5621a0e965579a0f14
MarkusVali <127192839+MarkusVali@users.noreply.github.com>
2024-04-08 10:41:22 +0300

    Initial commit

---
+++ FilmsApp/Services/TmdbService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class TmdbService
+    {
+        private const string ApiKey = "";
+        public const string TmdbHttpClientName = "TmdbClient";
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TmdbService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private HttpClient HttpClient => _httpClientFactory.CreateClient(TmdbHttpClientName);
+    }
+}
